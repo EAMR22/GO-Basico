c00fc47f4c97e8fc87d827a3f1d1b37b617dce9f
EAMR22 <elvis1042.rodriguez@gmail.com>
2022-03-23 22:32:55 -0600

    arrays y slices

--- src/main.go
+++ src/main.go
@@ -4,24 +4,31 @@ import "fmt"
 
 func main() {
 
-	// Defer:
-	defer fmt.Println("Hola") // Con defer ejecutade ultimo esta linea de codigo.
-	fmt.Println("Mundo")
+	// Array:
+	var array [4]int
+	array[0] = 1
+	array[1] = 2
 
-	// Continue y Break:
-	for i := 0; i < 10; i++ {
-		fmt.Println(i)
+	// Con "len" le dice cuantos elementos hay en el array.
+	// Con "cap" nos dice la capacidad maxima del array.
+	fmt.Println(array, len(array), cap(array))
 
-		// Continue:
-		if i == 2 {
-			fmt.Println("Continua el ciclo en", i)
-			continue
-		}
+	// Slice:
+	slice := []int{0, 1, 2, 3, 4, 5, 6}
+	fmt.Println(slice, len(slice), cap(slice))
 
-		// Break:
-		if i == 7 {
-			fmt.Println("Para el ciclo en", i)
-			break
-		}
-	}
+	// Metodos del slice:
+	fmt.Println(slice[0])   // Imprime el elemento 0.
+	fmt.Println(slice[:3])  // Imprime desde el inicio hasta el 2, el 3 no porque es exclusivo.
+	fmt.Println(slice[2:4]) // Imprime del 2 hasta el 3, el 4 no porque es exclusivo.
+	fmt.Println(slice[4:])  // Imprime del 4 en adelante, va desde el 4 porque es inclusivo.
+
+	// Append:
+	slice = append(slice, 7) // Con append agregaun elemento a la lista.
+	fmt.Println(slice)
+
+	// Append nueva lista:
+	newSlice := []int{8, 9, 10}
+	slice = append(slice, newSlice...) // Aqui une las 2 listas.
+	fmt.Println(slice)
 }
