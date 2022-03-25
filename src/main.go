ca0abf8b2f08662cbfcfbb301f6642301d118c86
EAMR22 <elvis1042.rodriguez@gmail.com>
2022-03-25 00:33:30 -0600

    structs y punteros

--- src/main.go
+++ src/main.go
@@ -1,16 +1,45 @@
 package main
 
 import (
-	pk "curso_golang_platzi/src/mypackage"
 	"fmt"
 )
 
+type pc struct {
+	ram   int
+	disk  int
+	brand string
+}
+
+func (myPC pc) ping() { // myPC actua como puntero
+	fmt.Println(myPC.brand, "Pong")
+}
+
+func (myPC *pc) duplicateRAM() {
+	myPC.ram = myPC.ram * 2
+}
+
 func main() {
 
-	var myCar pk.CarPublic
-	myCar.Brand = "Ferrari"
-	myCar.Year = 2020
-	fmt.Println(myCar)
+	a := 50
+	b := &a // Accedemos a la memoria de a.
 
-	pk.PrintMessage("Hola Platzi")
+	fmt.Println(b)
+	fmt.Println(*b) // Accedemos al valor de la direccion de memoria.
+
+	*b = 100
+	fmt.Println(a) // Cambia el valor de "a" en 100.
+
+	myPC := pc{ram: 16, disk: 200, brand: "msi"}
+	fmt.Println(myPC)
+
+	myPC.ping()
+
+	fmt.Println(myPC)
+	myPC.duplicateRAM()
+
+	fmt.Println(myPC)
+	myPC.duplicateRAM()
+
+	fmt.Println(myPC)
+
 }
