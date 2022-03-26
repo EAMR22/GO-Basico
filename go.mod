7ef59e1e65b5270daeb354b192a559706dcfa701
EAMR22 <elvis1042.rodriguez@gmail.com>
2022-03-26 11:08:52 -0600

    Go modules: Ir mas alla del GoPath con Echo

--- go.mod
+++ go.mod
@@ -1,3 +1,23 @@
 module curso_golang_platzi
 
 go 1.18
+
+require (
+	github.com/labstack/echo v3.3.10+incompatible // indirect
+	github.com/labstack/echo/v4 v4.7.2 // indirect
+	github.com/labstack/gommon v0.3.1 // indirect
+	github.com/mattn/go-colorable v0.1.12 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasttemplate v1.2.1 // indirect
+	github.com/yuin/goldmark v1.3.5 // indirect
+	golang.org/x/crypto v0.0.0-20220321153916-2c7772ba3064 // indirect
+	golang.org/x/mod v0.4.2 // indirect
+	golang.org/x/net v0.0.0-20220325170049-de3da57026de // indirect
+	golang.org/x/sys v0.0.0-20220325203850-36772127a21f // indirect
+	golang.org/x/text v0.3.7 // indirect
+	golang.org/x/tools v0.1.3-0.20210525215409-a3eb095d6aee // indirect
+	golang.org/x/tour v0.1.0 // indirect
+	golang.org/x/website/tour v0.0.0-20210616181959-e0d934b43647 // indirect
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
+)
--- src/main.go
+++ src/main.go
@@ -1,41 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
 
-func message(text string, c chan string) {
-	c <- text
-}
+	"github.com/labstack/echo"
+)
 
 func main() {
-	c := make(chan string, 2)
 
-	c <- "mensaje 1"
-	c <- "mensaje 2"
+	// Instanciar echo
+	e := echo.New()
 
-	fmt.Println(len(c), cap(c)) // len() indica cuantas go routines hay en ese channel.
-	// cap() nos indica cuanta es la cantidad maxima que puede almacenar en ese channel.
-
-	// Range y Close:
-
-	close(c) // Le indica al rountime de go que va a cerrar el canal.
-
-	for message := range c {
-		fmt.Println(message)
-	}
-
-	// Select:
-
-	email1 := make(chan string)
-	email2 := make(chan string)
-	go message("mensaje1", email1) // Imprime primero
-	go message("mensaje2", email2) // Imprime despues
-
-	for i := 0; i < 2; i++ {
-		select {
-		case m1 := <-email1:
-			fmt.Println("Email recibido de email 1", m1) // Imprime de ultimo
-		case m2 := <-email2:
-			fmt.Println("Email recibido de email 2", m2) // Imprime primero
-		}
-	}
+	// Ruta
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello World")
+	})
+	e.Logger.Fatal(e.Start(":1323"))
 }
