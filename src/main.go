8bba3e3ea67022c5e314a1c86bf8455ce8b6721a
EAMR22 <elvis1042.rodriguez@gmail.com>
2022-03-25 21:40:55 -0600

    channels

--- src/main.go
+++ src/main.go
@@ -1,33 +1,17 @@
 package main
 
-import (
-	"fmt"
-	"sync"
-	"time"
-)
+import "fmt"
 
-func say(text string, wg *sync.WaitGroup) {
-
-	defer wg.Done()
-
-	fmt.Println(text)
+func say(text string, c chan<- string) {
+	c <- text // Le indicamos que vamos a ingresar un dato a ese canal.
 }
 
 func main() {
-	var wg sync.WaitGroup // Acumula un conjunto de go routines y la va liberando poco a poco.
+	c := make(chan string, 1)
 
 	fmt.Println("Hello")
-	wg.Add(1) // Agregamos una go routine.
 
-	go say("world", &wg)
+	go say("Bay", c)
 
-	wg.Wait() // Le decimos al main que espere a las go routines de WaitGroup a que finalice.
-
-	// Funcion anonima:
-
-	go func(text string) {
-		fmt.Println(text)
-	}("Adios")
-
-	time.Sleep(time.Second * 1) // No es recomendable
+	fmt.Println(<-c) // La salida del dato del canal.
 }
